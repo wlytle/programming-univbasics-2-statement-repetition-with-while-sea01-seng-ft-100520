024632ed60e9126c61101c0403cccc9756c481ee
Will Lytle <wlytle89@gmail.com>
2020-08-10 17:46:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,10 @@
 count = 10
 while count >= 0 do
-  if count != 0 
-    puts count
-  else 
-    puts "Happy New Year!"
-  end
+  puts count
+  # if count != 0 
+  #   puts count
+  # else 
+  #   puts "Happy New Year!"
+  # end
   count--
 end
